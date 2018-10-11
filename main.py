19864c105784e80e86ad48435bdf603462852f18
student <tester@tester-m>
2018-10-11 21:34:51 +0200

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+IMIE = "Natalia"
+ILOSC = 10
+
+print("Hello Word" + IMIE + "Ona ma: " + str(ILOSC))
+
+zwierze = "psa"
+
+print("{0} ma {1}".format(IMIE, zwierze))
+
+samochody = ['syrena', 'polonez']
+print(samochody[0])
+
+for s in samochody:
+    print(s)
